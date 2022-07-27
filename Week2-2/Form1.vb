b381933b26ed7916203a9fc6f468b23a7c726459
Apriyandi <95987430+ZeinApriyandi@users.noreply.github.com>
2022-07-27 20:18:41 +0700

    Add files via upload

---
+++ Week2-2/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("Hello " + TextBox1.Text, Me.Text, MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+End Class
